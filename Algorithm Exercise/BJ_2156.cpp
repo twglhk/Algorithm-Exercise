897297d7dc5b9c687acfbf033e4621f241367bd5
신종하 <whdgkks12347@naver.com>
2018-08-28 17:23:00 +0900

    포도주 시식

--- Algorithm Exercise/BJ_2156.cpp
+++ Algorithm Exercise/BJ_2156.cpp
@@ -19,7 +19,7 @@ void Wine_DP()
 {
 	while (qDpContainer.size() != 1)
 	{
-		// No more can't attach other wines to vector.
+		// Can't attach other wines to vector.
 		if (qDpContainer.front().vWineIndex.back() >= n)
 		{
 			bool bIsBigger = false;
@@ -45,29 +45,36 @@ void Wine_DP()
 		else
 		{
 			// Wine structure's last index's number
-			int nLastNum = qDpContainer.front().vWineIndex.back() + 1;
+			int nLastNum = qDpContainer.front().vWineIndex.back();
+			vector<int>::iterator vIter = qDpContainer.front().vWineIndex.end()-1;
 
-			for (int i = nLastNum; i <= n; i++)
+			for (int i = nLastNum+1; i <= n; i++)
 			{
 				// If new number is put consecutively with past two ones, 
 				// skip creating a new structure and pusing the item.
-				if (nLastNum != 1 && nLastNum != 2)
+				if (qDpContainer.front().vWineIndex.size() >= 2)
 				{
-
+					if (*(vIter - 1) + 1 == *vIter && *vIter + 1 == i)
+					{
+						if (i == n)
+						{
+							Wine wTemp = qDpContainer.front();
+							wTemp.vWineIndex.push_back(i+1);
+							qDpContainer.push_back(wTemp);
+							break;
+						}
+							
+						else
+							continue;
+					}
 				}
+				
+				// Memorization
+				Wine wTemp = qDpContainer.front();
+				wTemp.vWineIndex.push_back(i);
+				wTemp.nTotalWineAmount += vWineVol[i];
 
-				if (qDpContainer.front().vWineIndex[nLastNum - 2] + 1 == qDpContainer.front().vWineIndex[nLastNum - 1] &&
-					qDpContainer.front().vWineIndex[nLastNum - 1] + 1 == qDpContainer.front().vWineIndex[nLastNum])
-					continue;
-		
-				else
-				{
-					Wine wTemp = qDpContainer.front();
-					wTemp.vWineIndex.push_back(i);
-					wTemp.nTotalWineAmount += vWineVol[i];
-
-					qDpContainer.push_back(wTemp);
-				}
+				qDpContainer.push_back(wTemp);
 			}
 		}
 
@@ -75,6 +82,10 @@ void Wine_DP()
 	}
 
 	// Print Maximum number
+	/*for (int i = 0; i < qDpContainer.front().vWineIndex.size(); i++)
+		cout << qDpContainer.front().vWineIndex[i] << " ";
+	cout << endl;*/
+
 	cout << qDpContainer.front().nTotalWineAmount << endl;
 }
 
@@ -91,11 +102,18 @@ int main()
 		vWineVol.push_back(nTemp);
 		
 		Wine wTemp;
-		wTemp.vWineIndex.resize(1, 0);
 		wTemp.vWineIndex.push_back(i);
 		wTemp.nTotalWineAmount = nTemp;
 		qDpContainer.push_back(wTemp);
 	}
+
+	/*cout << "WINE VOL" << endl;
+	for (int i = 0; i < vWineVol.size(); i++)
+	{
+		cout << vWineVol[i] << " ";
+	}
+	cout << endl;
+	cout << "------------" << endl*/;
 
 	Wine_DP();
 
--- Algorithm Exercise/연습장.cpp
+++ Algorithm Exercise/연습장.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-	string s = "ABCDE";
-	string temp = s.substr(0, 3);
-	cout << s << " " << temp << endl;
+	vector<int> vec;
+
+	vec.push_back(10);
+	vec.push_back(20);
+	vec.push_back(30);
+
+	vector<int>::iterator vIter = vec.end();
+
+	//printf("%x\n", vIter);
+	cout << *(vIter-1) << endl;
+	//cout << *(--vIter) << endl;
+	//cout << *(vIter - 1) << endl;
+
+	//vIter -= 1;
+
+	//cout << *vIter + 1<< endl;
 
 	return 0;
 }
